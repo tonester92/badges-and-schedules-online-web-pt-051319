c2025cfd8c83fbc747a93da2aa4f1a01ad76d698
Antonio Blandin <antonio.blandin25@gmail.com>
2019-05-18 17:59:10 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,11 @@ end
 def batch_badge_creator(array)
   array.collect {|a|"Hello my name is " + a}
 end
+
+def batch_badge_creator(arr)
+  new_array = []
+  arr.each do |name|
+    new_array.push("Hello, my name is #{name}.")
+  end
+  return new_array
+end
