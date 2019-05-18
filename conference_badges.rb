4727a848127c20b2470c445824351ba20ff5f705
Antonio Blandin <antonio.blandin25@gmail.com>
2019-05-18 18:00:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,3 +10,22 @@ def batch_badge_creator(arr)
   return new_array
 end
 
+def assign_rooms(arr)
+  new_array = []
+  counter = 1
+  arr.each do |name|
+  new_array.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+  counter+=1
+  end
+  return new_array  
+end
+
+def printer(arr)
+  batch_badge_creator(arr).each do |badge|
+    puts badge
+  end
+
+  assign_rooms(arr).each do |assignment|
+    puts assignment
+  end
+end 
