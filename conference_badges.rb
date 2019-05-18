779eee931846242e94b403f2a133b2030f09b47e
Antonio Blandin <antonio.blandin25@gmail.com>
2019-05-18 17:59:46 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,10 +2,6 @@ def badge_maker(name)
   puts "Hello, my name is #{name}"
 end
 
-def batch_badge_creator(array)
-  array.collect {|a|"Hello my name is " + a}
-end
-
 def batch_badge_creator(arr)
   new_array = []
   arr.each do |name|
